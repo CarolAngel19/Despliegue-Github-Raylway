e0d68085685f77a503951f9d4502161c8c31c183
carolangel19 <102700849+CarolAngel19@users.noreply.github.com>
2024-02-19 11:28:32 -0500

    Despliegue App Spring Boot con Github y Raylway.app

---
+++ src/main/java/com/example/DespliegueGithubRaylway/DespliegueGithubRaylwayApplication.java
@@ -0,0 +1,13 @@
+package com.example.DespliegueGithubRaylway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespliegueGithubRaylwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespliegueGithubRaylwayApplication.class, args);
+	}
+
+}
